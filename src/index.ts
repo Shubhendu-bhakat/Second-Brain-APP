28b0c956637f2a300f283c2442bc8f6984e84a0c
Shubhendu Bhakat <kumarsubho2003@gmail.com>
2025-07-01 02:12:04 +0530

    fixed the hash issue and completed signin ,signup routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, {Request,Response} from "express";
 import z from "zod";
 import dotenv from "dotenv";
+import bcrypt from "bcryptjs"
 import { connectToDb } from "./connectDB";
 import { userModel } from "./models/user";
 import { UserInput, userSchema } from "./models/zodSchema";
+import jwt from "jsonwebtoken";
 
+const JWT_PASS = "1234@";
 dotenv.config();
 const app = express();
 //app uses 
@@ -31,7 +34,11 @@ app.post("/app/v1/signup", async (req:Request, res:Response):Promise<any> => { /
 
     // Step 3 - Save new user
     const { userName, password } = validatedata;
-    await userModel.create({ userName, password });
+    //step 5 hashing password 
+    const saltRounds = 10;
+    const hashedPass:string = await bcrypt.hash(password, saltRounds);//saltrounds is necesary 
+    console.log(hashedPass);
+    await userModel.create({ userName:userName, password:hashedPass });
 
     return res.status(200).json({ message: "User signed Up" });
   } catch (err) {
@@ -44,8 +51,22 @@ app.post("/app/v1/signup", async (req:Request, res:Response):Promise<any> => { /
   }
 });
 
-app.post("/app/v1/signin",(req,res)=>{
-
+app.post("/app/v1/signin", async (req:Request,res:Response):Promise<any>=>{
+    const {userName , password } = req.body;
+    console.log(userName);
+    console.log(password);
+    const userExists = await userModel.findOne({userName:userName});
+    console.log(userExists);
+    if(!userExists  || !userExists.password){
+       return res.status(401).json({message:"Invalid username "})
+    }
+    const passwordMatch = await bcrypt.compare(password , userExists.password);
+    if(!passwordMatch)return res.status(401).json({message:"Invalid password "});
+    //sucess login 
+    const token = jwt.sign({
+        id:userExists._id
+    }, JWT_PASS , {expiresIn:"24h"});
+    return res.status(201).json({ message: "Login successfull " + token });
 });
 
 app.post("/app/v1/content",(req,res)=>{
